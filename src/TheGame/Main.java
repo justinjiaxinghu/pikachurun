e285656e3f69cd87509397bed361dd6deaa7c446
justinjhu <justinjiaxinghu@gmail.com>
2019-09-09 17:33:02 -0400

    glooms spawninng randomized

--- src/TheGame/Main.java
+++ src/TheGame/Main.java
@@ -1,7 +1,5 @@
 package TheGame;
-import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
-import javafx.animation.FadeTransition;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -15,15 +13,14 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.io.FileInputStream;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        long startTime = System.nanoTime()/1000000000;
 
         //create pikachu
         Image player = new Image(new FileInputStream("resources/pikachu.gif"));
@@ -57,8 +54,29 @@ public class Main extends Application {
         canvas.getChildren().addAll(pikachu, border, healthBar);
         Scene s = new Scene(canvas);
 
+        primaryStage.setScene(s);
+        primaryStage.show();
+
+
+        Random spawnTime = new Random();
+
+        ArrayList<ImageView> enemies = new ArrayList<>();
+
+        Timeline spawnEnemies = new Timeline(new KeyFrame(Duration.seconds(4), ev -> {
+            gloom[0] = new ImageView(obstacle);
+            gloom[0].setPreserveRatio(true);
+            gloom[0].setFitHeight(50);
+            gloom[0].setX(1100);
+            gloom[0].setY(450);
+            canvas.getChildren().add(gloom[0]);
+            enemies.add(gloom[0]);
+        }));
+        spawnEnemies.setCycleCount(Animation.INDEFINITE);
+        spawnEnemies.play();
+
         final int[] gravity = {0};
         s.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            double randomSpawnTime = spawnTime.nextInt(5) + 1;
             @Override
             public void handle(KeyEvent keyEvent) {
                 double yPos = pikachu.getY();
@@ -80,47 +98,39 @@ public class Main extends Application {
                                 }
                             }
                         };
+                        spawnEnemies.setRate(randomSpawnTime);
+                        randomSpawnTime = spawnTime.nextInt(8) + 2;
                         jump.start();
                     }
                 }
         });
 
-        primaryStage.setScene(s);
-        primaryStage.show();
-
         //spawn enemies and handle collisions
         AnimationTimer spawnEnemy = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                long curTime = l/1000000000;
-                if ((curTime - startTime) % 5 == 0) {
-                    gloom[0] = new ImageView(obstacle);
-                    gloom[0].setPreserveRatio(true);
-                    gloom[0].setFitHeight(50);
-                    gloom[0].setX(1100);
-                    gloom[0].setY(450);
-                    canvas.getChildren().add(gloom[0]);
-                }
-                gloom[0].setX(gloom[0].getX() - 10);
+                for (ImageView v : enemies) {
+                    v.setX(v.getX() - 10);
 
-                if (gloom[0].intersects(pikachu.getLayoutBounds())) {
-                    FadeTransition damageTaken = new FadeTransition(Duration.seconds(0.1), pikachu);
-                    damageTaken.setFromValue(1.0);
-                    damageTaken.setToValue(0.0);
-                    damageTaken.setCycleCount(Animation.INDEFINITE);
-                    damageTaken.play();
-                    new Timer().schedule(
-                            new TimerTask() {
-                                @Override
-                                public void run() {
-                                    pikachu.setOpacity(1.0);
-                                    damageTaken.stop();
-                                }
-                            },  500
-                    );
-                    gloom[0].setY(100000);
-                    canvas.getChildren().remove(gloom[0]);
-                    healthBar.setWidth(healthBar.getWidth() - 50);
+                    if (v.intersects(pikachu.getLayoutBounds())) {
+                        FadeTransition damageTaken = new FadeTransition(Duration.seconds(0.1), pikachu);
+                        damageTaken.setFromValue(1.0);
+                        damageTaken.setToValue(0.0);
+                        damageTaken.setCycleCount(Animation.INDEFINITE);
+                        damageTaken.play();
+                        new Timer().schedule(
+                                new TimerTask() {
+                                    @Override
+                                    public void run() {
+                                        pikachu.setOpacity(1.0);
+                                        damageTaken.stop();
+                                    }
+                                }, 500
+                        );
+                        v.setY(100000);
+                        canvas.getChildren().remove(v);
+                        healthBar.setWidth(healthBar.getWidth() - 50);
+                    }
                 }
             }
         };
